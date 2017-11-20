341048ba347163ee2c639b790c4e62b7a207d205
Witold Wysota <wysota@wysota.org>
2017-11-20 06:25:36 +0100

    Added license info

--- web/mlpipeline/jsutils/3rdparty/download.js
+++ web/mlpipeline/jsutils/3rdparty/download.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016-2017 Flatiron Institute, Simons Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 //download.js v4.2, by dandavis; 2008-2016. [CCBY2] see http://danml.com/download.html for tests/usage
 // v1 landed a FF+Chrome compat way of downloading strings to local un-named files, upgraded to use a hidden frame and optional mime
 // v2 added named files via a[download], msSaveBlob, IE (10+) support, and window.URL support for larger+faster saves than dataURLs
